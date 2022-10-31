8693a2a5e8b51e0dcb1058048240d4d81c105944
saggarr <saggarr.1993@gmail.com>
2022-10-31 20:25:40 +0600

    Initial commit

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LARVENDER KF125 Drone',
+        productNumber: '22485',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Ruko F11 Drone',
+        productNumber: '65782',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone With Camera Drone',
+        productNumber: '55685',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4K Drone',
+        productNumber: '22232',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: '4K Drone',
+        productNumber: '35344',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'K Drone',
+        productNumber: '55555',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
